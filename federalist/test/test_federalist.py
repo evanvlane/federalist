7bf90f93a46048612eed58518745fc5e0842debb
Evan Lane <evanvlane@gmail.com>
2015-03-14 01:52:36 -0400

    Added testing structure, style folder.

---
+++ federalist/test/test_federalist.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class FederalistTest(unittest.TestCase):
+
+    def test(self):
+        """Placeholder test."""
+
+        self.assertTrue(True)
+
+if __name__ == "__main__":
+    unittest.main()
