0b83854fdcc024dcb9ecb0152be78aa78087c1df
Evan Lane <evanvlane@gmail.com>
2015-03-14 21:39:08 -0400

    Fleshing out testing layout.

--- federalist/tests/test_federalist.py
+++ federalist/tests/test_federalist.py
@@ -1,12 +1,20 @@
+"""
+This is the testing framework for
+
+"""
 import unittest
 
 
 class FederalistTest(unittest.TestCase):
-
-    def test(self):
-        """Placeholder test."""
-
-        self.assertTrue(True)
-
+    """Doc string for unit test"""
+    
+    # TODO Implement test for loading Gutenberg URI
+    # TODO Implement test for failing loading non-Gutenberg URI
+    # TODO Implement test for loading local Gutenberg file
+    # TODO Implement test for failing local non-Gutenberg file
+    # TODO Implement test for loading css
+    # TODO Implement test for failing non-css
+    # TODO Implement test for stripping Gutenberg test
+    
 if __name__ == "__main__":
     unittest.main()
